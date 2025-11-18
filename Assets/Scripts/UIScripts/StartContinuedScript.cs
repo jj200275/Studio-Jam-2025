28a944be9e3a9ef566d576515a12684fc1e72da4
smiles1313 <46733232+smiles1313@users.noreply.github.com>
2025-11-17 18:08:40 -0800

    Intro UI done

--- Assets/Scripts/UIScripts/StartContinuedScript.cs
+++ Assets/Scripts/UIScripts/StartContinuedScript.cs
@@ -1,35 +1,20 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 using System.Collections;
 
-public class StartDialogue : MonoBehaviour
+public class StartContinuedScript : MonoBehaviour
 {
-    [Header("Canvas Groups")]
-    public CanvasGroup currentCanvasGroup;
-    public CanvasGroup nextCanvasGroup; 
-
-    [Header("Main UI Objects")]
-    public Image mainImage;          // this will switch from bottle to letter
     public Image blackScreen;        // use this to fade screen
-
-    [Header("Sprites")]
-    public Sprite bottleSprite;
-    public Sprite letterZeroSprite;
-
-    [Header("Dialogue")]
     public TMP_Text dialogueText;
     public Button continueButton;
 
     private void Start()
     {
-        // initialize transparencies
-        Fade.setAlpha(mainImage, 0);
         Fade.setAlpha(dialogueText, 0);
-
         blackScreen.color = Color.black;   // start black
         continueButton.gameObject.SetActive(false);
-
         StartCoroutine(Sequence());
     }
 
@@ -37,47 +22,38 @@ public class StartDialogue : MonoBehaviour
     {
         yield return new WaitForSeconds(1f);  // black screen
 
-        // reveal bottle
-        mainImage.sprite = bottleSprite;
-        yield return Fade.fadeGraphic(mainImage, 1f, 1);
-
-        // start dialogue
-        dialogueText.text = "\"What's this?\"";
-        yield return Fade.fadeGraphic(dialogueText, 0.5f, 1);
-        yield return new WaitForSeconds(2f);
-        yield return WaitForPlayer();  // to click
-        yield return Fade.fadeGraphic(mainImage, 0.5f, 0);
-        yield return Fade.fadeGraphic(dialogueText, 0.5f, 0);
-
-        // switch sprite to Letter 0
-        mainImage.sprite = letterZeroSprite;
-        yield return Fade.fadeGraphic(mainImage, 1f, 1);
-        yield return new WaitForSeconds(3f);
-        yield return Fade.fadeGraphic(mainImage, 1f, 0);
-
         // auto dialogue sequence
         string[] lines = {
-            "\"Is that...\"",
-            "\"Is that Seth???\"",
-            "\"Oh no. It looks like he and his friends have been abducted.\"",
-            "\"I've got to find a way to save them.\""
+            "\"But how can I possibly help? I feel so ill-prepared.\"",
+            "\"I've been doing a lot of diving recently. I've been seeing some interesting items in the depths.\"",
+            "\"Alright. Starting tomorrow, I'll explore and bring back anything I find helpful.\"",
         };
 
         foreach (string line in lines)
         {
             dialogueText.text = line;
             yield return Fade.fadeGraphic(dialogueText, 0.5f, 1);
-            // yield return WaitForPlayer();
-            yield return new WaitForSeconds(2.5f);
+            yield return new WaitForSeconds(3.5f);
             yield return Fade.fadeGraphic(dialogueText, 0.5f, 0);
         }
+
+        // player click here bc important - only ONE ITEM
+        dialogueText.text = "\"The only problem is... I can only carry <b>one item</b> per dive.\"";
+        yield return Fade.fadeGraphic(dialogueText, 0.5f, 1);  // fade in text
+        yield return new WaitForSeconds(2f);
+        yield return WaitForPlayer();  // to click
+        yield return Fade.fadeGraphic(dialogueText, 0.5f, 0);  // fade out text
+
+        dialogueText.text =  "\"I'll have to choose wisely.\"";
+        yield return Fade.fadeGraphic(dialogueText, 0.5f, 1);  // fade in text
+        yield return new WaitForSeconds(2f);
+        yield return Fade.fadeGraphic(dialogueText, 0.5f, 0);  // fade out text
 
         // fade full screen to black
         yield return StartCoroutine(Fade.fadeScreen(blackScreen, 1f));
 
-        // switch canvas groups
-        currentCanvasGroup.gameObject.SetActive(false);
-        nextCanvasGroup.gameObject.SetActive(true);
+        // go to next scene (Level 1 / start of playable game)
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
 
@@ -105,4 +81,3 @@ public class StartDialogue : MonoBehaviour
         continueButton.gameObject.SetActive(false);
     }
 }
-
